f955891c5268f963080f7d5bc47e35bbf569d738
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-02-24 16:31:23 -0300

    feat: init project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='domino',
+    version='1.0.0',
+    packages=[''],
+    url='https://github.com/idylicaro/domino',
+    license='',
+    author='Idyl Icaro',
+    author_email='idylicaro.se@gmail.com',
+    description="Dominos's Game"
+)
