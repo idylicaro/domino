0963089f9943ab86c57148b362a0b489d3a9f472
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-02-24 18:08:59 -0300

    feat: create piece class and ensure get piece and flip piece

---
+++ src/pieces/Piece.py
@@ -0,0 +1,40 @@
+class Piece(object):
+    """
+    This Class representing a piece on game
+    ...
+    Attributes
+    ----------
+    side_left : int
+        value of Left side of piece (0-6)
+    side_right : int
+        value of Left side of piece (0-6)
+
+    Methods
+    -------
+    get()
+        Return a tuple with values of piece
+    flip():
+        Turn over side left with side right
+    """
+
+    def __init__(self, side_left, side_right):
+        """
+            Parameters
+            ----------
+            side_left : int
+                value of Left side of piece (0-6)
+            side_right : int
+                value of Left side of piece (0-6)
+        """
+        self.side_left = side_left
+        self.side_right = side_right
+
+    def get(self):
+        """ Return a tuple with values of piece """
+        return self.side_left, self.side_right
+
+    def flip(self):
+        """ Turn over side left with side right """
+        temp = self.side_left
+        self.side_left = self.side_right
+        self.side_right = temp
